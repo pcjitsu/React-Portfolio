d8ac2cf50dbda3de24bad496df73eaac713fe9d8
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-10-28 18:23:52 -0500

    starting building framework for rest of application

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Home } from './pages/Home'
+import { Portfolio } from './pages/Portfolio'
+import {Resume} from './pages/resume'
+import { Contact } from './pages/contact'
+import { Navbar } from './Components/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState("Home")
+
+const onPageChange=()=> {
+  if(page == "Home"){
+    return <Home/>
+  }else if(page == "Portfolio"){
+    return <Portfolio/>
+  }else if(page == "Resume"){
+    return <Resume/>
+  }else{
+    return <Contact/>
+  }
+
+}
+
+
+
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+<Navbar setPage={setPage}/>
+{onPageChange()}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
