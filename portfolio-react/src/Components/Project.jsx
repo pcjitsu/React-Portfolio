ab4da158d0e6880bbf4dd029d75fb3497bdbd2a8
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-10-29 18:21:22 -0500

    project build out

--- portfolio-react/src/Components/Project.jsx
+++ portfolio-react/src/Components/Project.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export const Project = () => {
+
+
+
+
+// export const Project = (props) => {
+//   return (
+//     <div>Project</div>
+//   )
+// }
+
+
+export const Project= ({ projects }) => {
   return (
-    <div>Project</div>
-  )
-}
+    <div className="project-list">
+      {projects.map(project => (
+        <div className="project-tile" key={project.id}>
+          <a href={project.link}>
+            <img src={project.image} alt={project.title} />
+          </a>
+          <h2>{project.title}</h2>
+          <p>
+            <a href={project.repo}>Repository</a>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
--- portfolio-react/src/pages/Portfolio.jsx
+++ portfolio-react/src/pages/Portfolio.jsx
@@ -1,5 +1,55 @@
 import React from 'react'
 
+
+const projects = [
+  {
+    id:0,
+    title: "Code Quiz",
+    image: "/public/img/Code Quiz.png",
+    repo:"https://github.com/pcjitsu/Code-Quiz",
+    link:"https://pcjitsu.github.io/Code-Quiz/",
+  },
+  {
+    id:1,
+    title: "PassWord Generator",
+    image: "/public/img/Password-Creator-Mock.png",
+    repo:"https://github.com/pcjitsu/Password-Creator/blob/main/README.md" ,
+    link:"https://pcjitsu.github.io/Password-Creator/",
+  },
+  {
+    id:2,
+    title: "Vanilla Portfolio",
+    image: "/public/img/Vanilla-Portfolio.png",
+    repo:"https://github.com/pcjitsu/AGZikos-Portfolio" ,
+    link:"https://pcjitsu.github.io/AGZikos-Portfolio/",
+  },
+  {
+    id:3,
+    title: "Harmony Hub",
+    image: "/public/img/Harmony-Hub.png",
+    repo:"https://github.com/Jocy99/Harmony-Hub" ,
+    link:"https://salty-reaches-51385-c76877b9c210.herokuapp.com/",
+  },
+  {
+    id:4,
+    title: "Weather Check",
+    image: "/public/img/Weather-Check.png",
+    repo:"https://github.com/Jocy99/Weather-Check" ,
+    link:"https://pcjitsu.github.io/weather-Check/",
+  },
+  {
+    id:5,
+    title: "Schedule Maker",
+    image: "/public/img/Schedule.png",
+    repo:"https://github.com/pcjitsu/Calender-Application" ,
+    link:"https://pcjitsu.github.io/Calender-Application/",
+  },
+]
+
+
+
+
+
 export const Portfolio = () => {
   return (
     <div>Portfolio</div>
