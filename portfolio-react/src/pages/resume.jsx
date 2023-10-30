2d11e9680566536675632f727bcd58fdc8e68444
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-10-29 20:16:43 -0500

    added resume section

--- portfolio-react/src/pages/resume.jsx
+++ portfolio-react/src/pages/resume.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
 
-export const Resume = () => {
+
+const Resume = () => {
   return (
-    <div>R</div>
-  )
-}
+    <div>
+      <h2>EDUCATION</h2>
+      <p>United States Naval Academy, Annapolis, Maryland</p>
+      <p>B.S., English, 2013</p>
+
+      <h2>May 2022-Current: Growth Business Revenue Cloud Account Executive (Salesforce)</h2>
+      <p>
+        - Lead Enablement for Mid-Market Communication, Media, and Technology in Texas, Growth Texas, Growth Atlanta, and Growth New York on how to sell to the office of the CFO
+      </p>
+      <p>
+        - Lead enablement for all of Growth, Customer Business Services, and Manufacturing Revenue Cloud Teams (Growth to Enterprise) on how to position Manufacturing Industry Solutions.
+      </p>
+      <p>
+        - Lead Enablement for Revenue Cloud Growth and Mid-Market Solution Engineers on how to demo Manufacturing Cloud
+      </p>
+      <p>Sold first Manufacturing Cloud deal in all of Growth Revenue Cloud</p>
+      <p>122% of Annual Quota</p>
+      <p>Revenue Cloud Growth Business Deal of the Year recipient</p>
+
+      <h2>May 2021-May 2022: SMB Core Account Executive (Salesforce)</h2>
+      <p>Rookie Rockstar: 141% to Full Quota in May</p>
+      <p>100K Club December: 115.26K Closed on 36.5K Quota</p>
+      <p>100K Club January: 115.16K Closed on 36.5K Quota</p>
+      <p>Obtained 107% of Annual Quota</p>
+
+      <h2>December 2018 to May 2021: Project Manager/Superintendent (Mitsubishi, Inc.)</h2>
+      <p>Reviewed plans and inspected ongoing construction to keep work in line with project goals; completed 16 out of 18 projects under budget with remaining 2 at budget.</p>
+      <p>Turned project that was sold at $200,000 loss into the most profitable project of 2020 by $600,000.</p>
+
+      <h2>June 2018 to November 2018: Emergency Medical Technician (McCormick Ambulance)</h2>
+      <p>Provided emergency care to the citizens of Compton, Hawthorne, and Inglewood California. Averaged over 40 calls a week.</p>
+
+      <h2>July 2014 to May 2018: Battalion Operation Officer/Platoon Commander (Marine Corps)</h2>
+      <p>Managed $32,000 in liquid assets as the sole forward operations funds manager; enabled platoon to operate independently and reduced the burden of support from Coalition Joint Task Force-Erbil during operations to clear ISIS from the city of Mosul.</p>
+      <p>Planned operations, resourced and directed 3 elements that executed 40+ independent patrols and reported 400+ hours of surveillance; provided data vital to building atmospherics for follow-on missions and developing courses of action.</p>
+      <p>Directed training/led a 52-person unit to independently recover sensitive material from a downed unmanned aerial vehicle; successfully executed a time-sensitive mission, ensuring the safety of critical material from being utilized by hostile forces.</p>
+
+      <h2>ADDITIONAL INFORMATION</h2>
+      <p>Technical Proficiencies: Javascript, Node, SQL, MongoDB, HTML, CSS, React, Express, Salesforce CRM, and CPQ</p>
+      <p>Other: Navy-Marine Corps Commendation Medal; BJJ Brown Belt; EMT Certification; Full Stack Web Dev Bootcamp</p>
+    </div>
+  );
+};
+
+export default Resume;
